c784da670f1a43a04cbad8d757b427e589e1209a
AlexTey99 <teyalex99@gmail.com>
2024-07-13 10:57:20 +0200

    se elimino el react-router que no se estaba usando

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MainContainer } from './container/MainContainer';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
@@ -10,11 +9,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    <Router>
-      <Routes>
-        <Route path='/' element={<MainContainer />} />
-      </Routes>
-    </Router>
+    <MainContainer />
   </Provider>
 );
 
